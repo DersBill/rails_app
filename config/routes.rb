1bbc750981a0d1d98aa96d24c9e9a2b1302b4831
Anders Bill <anders@darkroom.tech>
2018-04-12 21:49:53 +0000

    added new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   root 'welcome#home'
   get 'about', to: 'welcome#about'
   
+  resource :articles
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
